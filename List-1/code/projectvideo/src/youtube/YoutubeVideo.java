5d42689a01dae56433546a4865770b9f31611732
Ramon Almeida <ramongoncalves76@gmail.com>
2022-09-13 00:29:53 -0300

    Feat: exerc. vídeo

---
+++ List-1/code/projectvideo/src/youtube/YoutubeVideo.java
@@ -0,0 +1,19 @@
+package youtube;
+
+public class YoutubeVideo {
+
+    private final int[] bits;
+
+    public YoutubeVideo() {
+        bits = new int[] {
+                80, 97, 114, 97, 98, 233, 110, 115, 44,
+                32, 118, 99, 32, 99, 111, 110, 115, 101,
+                103, 117, 105, 117, 46
+        };
+    }
+
+    public int[] getBits() {
+        return bits;
+    }
+
+}
