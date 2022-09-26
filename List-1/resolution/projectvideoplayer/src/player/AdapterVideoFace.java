7e43721243fcda929bb52c20545afd65bd3d9631
Ramon Almeida <ramongoncalves76@gmail.com>
2022-09-26 12:13:10 -0300

    Feat: refactoring AdapterVideoFace

--- List-1/resolution/projectvideoplayer/src/player/AdapterVideoFace.java
+++ List-1/resolution/projectvideoplayer/src/player/AdapterVideoFace.java
@@ -1,11 +1,15 @@
 package player;
 
 import face.FaceVideo;
-import java.lang.Math;
 
 public class AdapterVideoFace extends Video {
 
     private final FaceVideo faceVideo;
+
+    /**
+     * "Tabela" das potências de 2 no intervalo de 8bit=1Byte=256(Valores).
+     */
+    private static final int[] TABLE_TWOPOWERS_BYTE = { 128, 64, 32, 16, 8, 4, 2, 1 };
 
     public AdapterVideoFace(FaceVideo faceVideo) {
         this.faceVideo = faceVideo;
@@ -17,12 +21,13 @@ public class AdapterVideoFace extends Video {
         boolean[] bits = faceVideo.getBitsFace();
         int[] arrCode = new int[bits.length / 8];
 
-        for (int i = 0, valueBit = 0; i < bits.length; i++) {
-            valueBit = (int) Math.pow((double) 2, (double) (7 - i % 8)) * (bits[i] ? 1 : 0);
-            arrCode[i / 8] += valueBit;
-        }
+        for (int i = 0; i < bits.length; i++)
+            arrCode[i / 8] += TABLE_TWOPOWERS_BYTE[i % 8] * convertBooleanToBit(bits[i]);
 
         return arrCode;
     }
 
+    private int convertBooleanToBit(boolean v) {
+        return v ? 1 : 0;
+    }
 }
