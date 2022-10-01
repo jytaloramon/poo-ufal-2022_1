a2ec9e44a5b37925acdb560518e80e68c2b41510
Franklyn Roberto <58437929+hadesfranklyn@users.noreply.github.com>
2022-10-01 13:23:30 -0300

    Add files via upload

---
+++ List-1/resolution/exercicio_4/entities/enums/Color.java
@@ -0,0 +1,7 @@
+package exercicio_4.entities.enums;
+
+public enum Color {
+	BLACK,
+	BLUE,
+	RED;
+}
---
+++ List-1/resolution/exercicio_5/entities/enums/OrderStatus.java
@@ -0,0 +1,8 @@
+package exercicio_5.entities.enums;
+
+public enum OrderStatus {
+	PENDING_PAYMENT,
+	PROCESSING,
+	SHIPPED,
+	DELIVERED
+}
